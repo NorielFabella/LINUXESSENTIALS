75f9a145a7144c829904a7aecab36449fa28967f
Noriel Omar Fabella <152840848+NorielFabella@users.noreply.github.com>
2025-02-05 01:45:24 +0000

    Final commit

---
+++ Activity1.sh
@@ -0,0 +1,12 @@
+NAME=""
+AGE=0
+PROGRAM=""
+
+echo "ENTER NAME:"
+read NAME
+echo "ENTER YOUR AGE:"
+read AGE
+echo "ENTER PROGRAM:"
+read PROGRAM
+
+echo "HELLO, $NAME! THIS IS YOUR $AGE! & YOUR PROGRAM IS $PROGRAM"
